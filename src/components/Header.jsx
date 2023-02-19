1e9978b3a0f1d7fe7d154bed85236f022b381a4b
ellzmarie <elliematutisdigal@gmail.com>
2023-02-19 14:03:17 -0800

    adds css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,18 @@ import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
-    <nav className="nav">
+    <>
+    <nav className="header">
       <Link to="/">
-        <div>theLOOP App</div>
+        <div className="app-title"><p className="app-name">t h e L O O P</p></div>
       </Link>
     </nav>
+
+    <div className="topnav">
+      <Link to="#">artists</Link>
+      <Link to="#">fashion</Link>
+    </div>
+    </>
   );
 }
 
